8449c9be420ef58f94e7d2abf2464857cc1c9e1e
Matt Crouch <matt@mattcrouch.net>
2018-07-01 20:21:23 +0100

    `Button` component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,9 +2,32 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { selectV2, text, withKnobs } from "@storybook/addon-knobs/react";
 
 import Button from ".";
 
-storiesOf("Button", module).add("default", () => (
-  <Button onClick={action("button clicked")}>This is a button</Button>
-));
+const sizes = {
+  Default: "",
+  Small: "small",
+  Large: "large"
+};
+
+const types = {
+  Default: "default",
+  Primary: "primary",
+  Danger: "danger"
+};
+
+storiesOf("Button", module)
+  .addDecorator(withKnobs)
+  .add("Basic Usage", () => {
+    const buttonText = text("Label", "This is Net Magazine");
+    const size = selectV2("Size", sizes, Object.values(sizes)[0]);
+    const type = selectV2("Type", types, Object.values(types)[0]);
+
+    return (
+      <Button onClick={action("button clicked")} size={size} type={type}>
+        {buttonText}
+      </Button>
+    );
+  });
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./styles.css";
 
-const Button = ({ children, onClick, type }) => {
+const Button = ({ children, onClick, size, type }) => {
   const classes = ["Button"];
 
   if (type === "primary") {
     classes.push("Button__primary");
   } else if (type === "danger") {
     classes.push("Button__danger");
+  }
+
+  if (size === "small") {
+    classes.push("Button__small");
+  } else if (size === "large") {
+    classes.push("Button__large");
   }
 
   return (
